30d4aa40c6cdaf02f683efdea7a5c30350ad9ec8
gohlivor <gohlivor@gmail.com>
2014-12-14 17:12:23 -0600

    added Oculus Rift Integration
    had issues importing ovr 4.4. had to drag in OVR folder from existing
    project.

---
+++ Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMove : MonoBehaviour 
+
+	{
+		public Transform ForwardRig;
+		public int Speed;
+		// Use this for initialization
+		void Start () 
+		{
+			
+		}
+		
+		// Update is called once per frame
+		void Update () 
+		{
+			this.transform.Translate(ForwardRig.forward * Speed * Time.deltaTime );
+		}
+	}
