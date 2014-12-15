b86bce0f1ee900a83638dec04f61f36d67fe4e0f
gohlivor <gohlivor@gmail.com>
2014-12-14 23:27:42 -0600

    added view scripts

---
+++ Assets/Scripts/viewscript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class viewscript : MonoBehaviour {
+
+	GameObject[] ourObjects;
+	Light theLight;
+	bool hiddenChanged = false;
+	int currentObject = 0;
+
+	// Use this for initialization
+	void Start () {
+		ourObjects = new GameObject[3];
+		ourObjects [0] = GameObject.Find ("theBox");
+		ourObjects [1] = GameObject.Find ("theSphere");
+		ourObjects [2] = GameObject.Find ("theCylinder");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Check if our current object is visible.
+		if (!ourObjects [currentObject].renderer.isVisible && !hiddenChanged) {
+			hiddenChanged = true;
+			Debug.Log ("changing object");
+			currentObject += 1;
+			if (currentObject == 3) {
+					currentObject = 0;
+			}
+
+			for (int i = 0; i < 3; i++)
+			{
+				if (i == currentObject)
+				{
+					ourObjects[i].renderer.enabled = true;
+				}
+				else
+				{
+					ourObjects[i].renderer.enabled = false;
+				}
+			}
+		} else if (ourObjects [currentObject].renderer.isVisible ) {
+			hiddenChanged = false;
+		}
+
+		
+	}
+}
---
+++ Assets/Scripts/viewscript2.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class viewscript2 : MonoBehaviour {
+
+	GameObject[] ourObjects;
+	Light theLight;
+	bool hiddenChanged = false;
+	int currentObject = 0;
+	
+	// Use this for initialization
+	void Start () {
+		ourObjects = new GameObject[2];
+		ourObjects [0] = GameObject.Find ("thisHedge");
+		ourObjects [1] = GameObject.Find ("thisArch");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Check if our current object is visible.
+		if (!ourObjects [currentObject].renderer.isVisible && !hiddenChanged) {
+			hiddenChanged = true;
+			Debug.Log ("changing object");
+			currentObject += 1;
+			if (currentObject == 2) {
+				currentObject = 0;
+			}
+			
+			for (int i = 0; i < 2; i++)
+			{
+				if (i == currentObject)
+				{
+					ourObjects[i].renderer.enabled = true;
+
+				}
+				else
+				{
+					ourObjects[i].renderer.enabled = false;
+					//GameObject.Destroy(ourObjects[0],2f);
+				}
+			}
+
+
+		} else if (ourObjects [currentObject].renderer.isVisible ) {
+			hiddenChanged = false;
+
+		}
+	}
+}
