13f4a1389aeb014a0d0feeb9a74f3520d6915655
gohlivor <gohlivor@gmail.com>
2014-12-16 02:22:16 -0600

    Added Xbox 360 Joystick Controller Input
    4th and 5th axis has a lot of drift. will handle with Oculus Rift.

---
+++ Assets/Scripts/viewscript3.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class viewscript3 : MonoBehaviour {
+
+    public GameObject[] ourObjects;
+    Light theLight;
+    bool hiddenChanged = false;
+    int currentObject = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check if our current object is visible.
+        if (!ourObjects[currentObject].renderer.isVisible && !hiddenChanged)
+        {
+            hiddenChanged = true;
+            Debug.Log("changing object");
+            currentObject += 1;
+            if (currentObject == ourObjects.Length)
+            {
+                currentObject = 0; //sets back to zero when the last object in array size is viewed
+            }
+
+            for (int i = 0; i < ourObjects.Length; i++)
+            {
+                if (i == currentObject)
+                {
+                    ourObjects[i].SetActive(true);
+                   
+
+                }
+                else
+                {
+                    ourObjects[i].SetActive(false); 
+                }
+            }
+
+
+        }
+        else if (ourObjects[currentObject].renderer.isVisible)
+        {
+            hiddenChanged = false;
+
+        }
+    }
+}
